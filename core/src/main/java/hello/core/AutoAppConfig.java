df3e6edd6c4e69b3a25070cd79237c8a17fb62a7
nhkiiim <nhkiiim@naver.com>
2023-03-14 22:32:26 +0900

    Add: 컴포넌트 스캔 basePackages 추가

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @ComponentScan(
+        basePackages = "hello.core",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // 학습하며 만들어둔 Configuration 필터링
 ) // @Component 애너테이션을 찾아가 자동으로 컨테이너에 등록해줌
 @Configuration
