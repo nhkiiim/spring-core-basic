6a72d737eff415a3e879810edd358e77c7263516
nhkiiim <nhkiiim@naver.com>
2023-03-17 21:31:35 +0900

    Add: 빈 중복 등록

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -10,5 +13,12 @@ import org.springframework.context.annotation.FilterType;
 ) // @Component 애너테이션을 찾아가 자동으로 컨테이너에 등록해줌
 @Configuration
 public class AutoAppConfig {
+
+    // 자동으로 등록되는 빈이랑 같은 이름으로 하나 생성
+    // Overriding bean definition
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 
 }
