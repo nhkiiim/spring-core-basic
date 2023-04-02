f92c19e9c69d8c4b8cb6aa5f3de8c0bd496ded85
nhkiiim <nhkiiim@naver.com>
2023-04-02 17:29:59 +0900

    Test: 싱글톤 빈과 프로토타입 빈 사용 시 문제점

---
+++ core/src/test/java/hello/core/scope/SingtonWIthPrototypeTest1.java
@@ -0,0 +1,81 @@
+package hello.core.scope;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SingtonWIthPrototypeTest1 {
+
+    @Test
+    void prototypeFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCount();
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.addCount();
+        assertThat(prototypeBean2.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(2);
+
+        // 프로토 타입 빈이지만 2를 반환하는 이유
+        // 싱글톤 빈에 프로토타입 빈을 주입하면 같은 빈을 사용하게 됨 (호출 시 마다 새로 생성해주는게 아니라 생성 시 한번만 호출)
+        // 프로토 타입 빈의 의도와 일치하지 않음
+    }
+
+    @Scope("singleton")
+    @RequiredArgsConstructor
+    static class ClientBean {
+        // 스프링 컨테이너에 요청 시 프로토타입 빈 생성 및 할당 (생성 시점에 프로토타입 빈 주입 - 계속 같은 빈 사용)
+        private final PrototypeBean prototypeBean;
+
+        public int logic() {
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+    }
+
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init" + this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+
+}
