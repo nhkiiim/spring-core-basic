f3f4e36690bc080b623c482b332ecdf03fef0b81
nhkiiim <nhkiiim@naver.com>
2023-04-03 22:40:12 +0900

    Refactor: ObjectProvider 사용

--- core/src/test/java/hello/core/scope/SingtonWIthPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingtonWIthPrototypeTest1.java
@@ -2,6 +2,8 @@ package hello.core.scope;
 
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -35,7 +37,7 @@ public class SingtonWIthPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
         // 프로토 타입 빈이지만 2를 반환하는 이유
         // 싱글톤 빈에 프로토타입 빈을 주입하면 같은 빈을 사용하게 됨 (호출 시 마다 새로 생성해주는게 아니라 생성 시 한번만 호출)
@@ -46,9 +48,13 @@ public class SingtonWIthPrototypeTest1 {
     @RequiredArgsConstructor
     static class ClientBean {
         // 스프링 컨테이너에 요청 시 프로토타입 빈 생성 및 할당 (생성 시점에 프로토타입 빈 주입 - 계속 같은 빈 사용)
-        private final PrototypeBean prototypeBean;
+        // private final PrototypeBean prototypeBean;
+
+        // ObjectProvider: prototype bean을 찾아줌 (ObjectFactory에서 몇가지 편의 기능 추가됨)
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
